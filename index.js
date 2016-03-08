f6f7f57a1afa435739c6ab99299c7973afae83bf
Luciano Bueno <glamero@gmail.com>
2016-03-08 18:21:48 -0300

    bump

---
+++ index.js
@@ -0,0 +1,128 @@
+var mootools = require ('mootools'),
+	path = require ('path'),
+	util = require('util'),
+	winston = require('winston');
+
+module.exports = new Class({
+  Implements: [Options, Events],
+  
+  logger: null,
+  
+  initialize: function(app, options){
+	
+// 	console.log(util.inspect(server,{depth: 10}));
+	
+	this.setOptions(options);
+// 	
+// 	this.logger = new (winston.Logger)({
+// 	  transports: [
+// // 		new (winston.transports.Console)(),
+// 		new (winston.transports.File)( {
+// 		  filename: path.resolve(options.path) + '/all_log',
+// // 		  handleExceptions: true
+// 		}),
+// 	  ],
+// 	  exitOnError: false
+// 	});
+	
+// 	this.logger.add('error', {
+// 	  file: {
+// 		filename: path.resolve(options.path) + '/error_log',
+// 	  },
+// 	  
+// 	});
+// 
+	this.logger = winston;
+// 	this.logger.exitOnError = false;
+	
+// 	console.log('logger');
+// 	console.log(this);
+// 	
+// 	
+	this.logger.loggers.options.transports = [
+	  new (winston.transports.File)( {
+		filename: path.resolve(options.path) + '/all_log',
+// 		handleExceptions: true,
+	  }),
+	];
+
+	
+// 	this.logger.loggers.add('error', {
+// 	  file: {
+// 		filename: path.resolve(options.path) + '/error_log',
+// 	  },
+// 	  
+// 	});
+	this.logger.loggers.add('error', {
+	  transports: [
+		new (winston.transports.File)( {
+		  filename: path.resolve(options.path) + '/error_log',
+  // 		handleExceptions: true,
+		}),
+	  ]
+	  
+	});
+
+// 	this.logger.loggers.add('access', {
+// 	  file: {
+// 		filename: path.resolve(options.path) + '/access_log'
+// 	  }
+// 	});
+	
+	this.logger.loggers.add('access', {
+	  transports: [
+		new (winston.transports.File)( {
+		  filename: path.resolve(options.path) + '/access_log',
+  // 		handleExceptions: true,
+		}),
+	  ]
+	  
+	});
+	
+// 	this.logger.loggers.add('profiling', {
+// 	  file: {
+// 		filename: path.resolve(options.path) + '/profile'
+// 	  }
+// 	});
+	this.logger.loggers.add('profiling', {
+	  transports: [
+		new (winston.transports.File)( {
+		  filename: path.resolve(options.path) + '/profiling_log',
+  // 		handleExceptions: true,
+		}),
+	  ]
+	  
+	});
+	
+	app['profile'] = function(string){
+	  this.logger.loggers.get('profiling').profile(string);
+	}.bind(this);
+	
+	app['log'] = function(name, type, string){
+	  
+// 	  console.log(this.logger.loggers.get('access'));
+	  
+	  if(!this.logger.loggers[name])
+		this.logger.loggers.add(name, {
+		  transports: [
+			new (winston.transports.File)( {
+			  filename: path.resolve(options.path) + '/' + name + '_log'
+			}),
+		  ]
+		});
+		
+	  this.logger.loggers.get(name).log(type, string);
+	}.bind(this);
+	
+// 	console.log(server);
+  },
+  //express middleware
+  access: function(){
+	return function access(req, res, next) {
+	  this.logger.loggers.get('access').log('info', req.method + ' ' + req.url  + ' - HTTP ' + req.httpVersion);
+	  return next();
+	}.bind(this);
+  }
+
+});
+
